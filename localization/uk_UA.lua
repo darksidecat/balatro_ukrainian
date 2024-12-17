8bae48f43d277ca42ebe91bb5caac4dee1fd0a2a
Damglador <vse.stopchanskyi@gmail.com>
2024-12-17 03:23:49 +0100

    Виправлення перекладу
    1. Викл/вкл - русизм
    2. Зіграні карти та подібні написи замінено на Зіграно карт для кращої відповідності контексту (показує скільки карт було зіграно/скинуто тощо не показуючи самі карти)
    3. Вибрати - Виберіть. Виберіть 1 та тому подібне, виглядає органічніше
    4. Високий бал - Рекорд, щоб була відповідність початковому значенню
    5. Виправлено "Виграйте забіши"

--- localization/uk_UA.lua
+++ localization/uk_UA.lua
@@ -3046,7 +3046,7 @@
             challenge_locked = {
                 name = "Заблоковано",
                 text = {
-                    "Виграйте забіши з хоча б",
+                    "Виграйте забіг з хоча б",
                     "#1# різними колодами, щоб розблокувати",
                     "Режим виклику",
                     "{C:attention,s:2}#2#/#1#"
@@ -3732,10 +3732,10 @@
             k_balanced = "Збалансовано",
             k_view = "Огляд",
             k_all_hands = "Усі руки",
-            k_high_score_ex = "Високий бал!",
+            k_high_score_ex = "Рекорд!",
             k_demo_version_ex = "Демо-версія!",
             k_playing_as = "Граючи за",
-            k_choose = "Вибрати",
+            k_choose = "Виберіть",
             k_achievements_disabled = "Досягнення вимкнені",
             k_trophies_disabled = "Трофеї вимкнені",
             ph_display_stickers = "Відображати стікери ставок під час забігу",
@@ -3778,10 +3778,10 @@
             ph_score_hand = "Найкраща рука",
             ph_score_poker_hand = "Найчастіше зіграна рука",
             ph_score_new_collection = "Нові відкриття",
-            ph_score_cards_played = "Зіграні карти",
-            ph_score_cards_discarded = "Скинуті карти",
+            ph_score_cards_played = "Зіграно карт",
+            ph_score_cards_discarded = "Скинуто карт",
             ph_score_times_rerolled = "Разів оновлено",
-            ph_score_cards_purchased = "Придбані картки",
+            ph_score_cards_purchased = "Придбано карт",
             ph_unscored_hand = "Рука не принесе очки",
             ph_alert_debuff_confirm = "Натисніть 'Грати' ще раз, щоб підтвердити",
             ml_demo_thanks_message = {
@@ -3814,20 +3814,20 @@
                 "Без рамок"
             },
             ml_vsync_opt = {
-                "VSync вкл.",
-                "VSync викл."
+                "VSync увімк..",
+                "VSync вимк."
             },
             ml_shadow_opt = {
-                "Вкл",
-                "Викл"
+                "Увімк",
+                "Вимк"
             },
             ml_smoothing_opt = {
-                "Викл",
-                "Вкл"
+                "Вимк",
+                "Увімк"
             },
             ml_bloom_opt = {
-                "Викл",
-                "Вкл"
+                "Вимк",
+                "Увімк"
             },
             ml_card_stats = {
                 "Карта",
